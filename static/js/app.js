df98ffe04b57aff33977d8f0e27bcd661b7bf46f
Belinda <bmapesa@yahoo.com>
2020-10-08 21:51:41 -0700

    second commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // Fetch the JSON data and console log it
-d3.json("samples.json").then(function(data) {
+    d3.json("samples.json").then(function(data) {
     //   console.log(data);
     var names = data["names"]
     // console.log(names)
@@ -13,11 +13,12 @@ d3.json("samples.json").then(function(data) {
     })
 
     buildDashboard(names[0])
-//
-});
+
+    });
 
 
-function buildDashboard(name){
+// create a  function key to update graphs
+ function buildDashboard (name){ 
     d3.json("samples.json").then(function(data) {
       
     
@@ -26,30 +27,22 @@ function buildDashboard(name){
     //  console.log(samples)
         var metadata = data.metadata.filter(sample => sample.id == name)[0]
         // console.log(metadata) 
-    // d3.json(`samples.${sample}`).then(function(data){
     
-    // get  top 10 sample values to plot
+        // get  top 10 sample values to plot
     var sampleValues = samples.sample_values.slice(0,10).reverse()
-    // console.log(`SampleValues:${sampleValues}`)
-    // console.log(samples[0])
+       // console.log(samples[0])
     
     // get top 10 OTU id's  to plot
     var topIdvalues = samples.otu_ids.slice(0,10)
     // console.log(`topIdvalues:${topIdvalues}`)
     
     // get OTUids
-    // array.map(function(currentValue, index, arr), thisValue)
-    
-    var otuIds = topIdvalues.map(data=> "OTU " + data).reverse()
+        var otuIds = topIdvalues.map(data=> "OTU " + data).reverse()
     // console.log(`otu_ids:${otuIds}`)
     
     // get labels
      var labels = samples.otu_labels.slice(0,10).reverse()
-    
-    //  var otuIds = ["OTU 1167","OTU 2859","OTU 482","OTU 2264","OTU 41", "OTU 1189", "OTU 352","OTU 189","OTU 2318","OTU 1977"]
-    //  var otuNums = [1167, 2859, 482, 2264,41, 1189, 352, 189, 2318,1977]
-    //  var sampleValues = [0, 50, 100, 150]
-    
+        
     // / Create the Trace
       var trace = {
       x: sampleValues,
@@ -77,11 +70,9 @@ function buildDashboard(name){
     
     // Plot the chart to a div tag with id "bar"
     Plotly.newPlot("bar", data, layout);
-    
-    // });
+        
     
     // create bubble chart
-    
     // create the trace for the bubble chart
     var otu_Ids = samples.otu_ids
     var samples_Values = samples.sample_values
@@ -122,7 +113,46 @@ function buildDashboard(name){
  // use  Object.entries to add each key and value pair to the panel
     Object.entries(metadata).forEach(function([key,value]) {
         demoInfo.append("panel-body").text(`${key}:${value} \n`);
-            
+
+
+// create gauge chart
+var metadatagc = data.metadatagc
+var wfreq = data.metadatagc.map(data => "wfreq" + data)
+console.log(`Washing Freq: ${wfreq}`)
+
+var data_gc= [
+	{
+		domain: { x: [0, 1], y: [0, 1] },
+		value: parseFloat(wfreq),
+		title: { text: "Weekly Washing Frequency" },
+		type: "indicator",
+        mode: "gauge+number",
+        
+        gauge:{axis:{range:[0-9]},
+                steps:[
+                    {range:[0-1], color:"cream"},
+                    {range:[1-2], color:"lightbrown"},
+                    {range:[2-3],color:"lime"},
+                    {range:[3-4],color:"lightgreen"},
+                    {range:[4-5],color:"brightgreen"},
+                    {range:[5-6],color:"brightgreen"},
+                    {range:[6-7],color:"green"},
+                    {range:[7-8],color:"darkgreen"},
+                    {range:[8-9],color:"darkgreen"},
+                ]}
+	}
+];
+
+var layout_gc = {
+     width: 700, 
+     height: 600,
+     margin: { t: 100, b:25, l:100, r:25 }
+     };
+Plotly.newPlot('gauge', data_gc, layout_gc);    
+
+
+
+
 
     })
 
@@ -132,7 +162,6 @@ function buildDashboard(name){
    
 function optionChanged (name){
     buildDashboard(name)
-
 }
 
 
